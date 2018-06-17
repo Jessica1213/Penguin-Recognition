11b35f2a4606513308b5abdd4db83fb542010eab
Jessica <jessica121353@gmail.com>
2018-06-18 01:10:15 +0800

    test

--- test.py
+++ test.py
@@ -7,9 +7,18 @@ Created on Sun Jun 17 20:21:42 2018
 """
 
 from PIL import Image
-im = Image.open("../CNNdataset/TRUE/T00021.jpg")
-im.show()
-print(im.size)
-im = Image.open("../CNNdataset/TRUE/T00022.jpg")
-im.show()
-print(im.size)
+image = Image.open("../CNNdataset/TRUE/T00035.jpg")
+try:    
+    exif=dict(image._getexif().items())
+    orientation = 274
+    if   exif[orientation] == 3 : 
+        image=image.rotate(180, expand=True)
+    elif exif[orientation] == 6 : 
+        image=image.rotate(270, expand=True)
+    elif exif[orientation] == 8 : 
+        image=image.rotate(90, expand=True)
+except:
+    pass
+image.show()
+print(image.size)
+
